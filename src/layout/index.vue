afc497bc9f7c43a52190b323cdba9ca9b6b95720
lerzi <569259131@qq.com>
2023-04-30 02:45:01 +0800

    feat:update

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -10,12 +10,13 @@ const { toggle } = useFullscreen(appMain)
     <n-layout has-sider position="absolute">
       <Sider></Sider>
       <n-layout>
-        <PageHeader @full="toggle"></PageHeader>
-        <n-layout-content ref="appMain">
-          <div p-6>
+        <div h-full flex flex-col>
+          <PageHeader @full="toggle"></PageHeader>
+          <div w-full flex-1 bg-gray-2:50 p-6>
             <router-view />
           </div>
-        </n-layout-content>
+        </div>
+
       </n-layout>
     </n-layout>
   </div>
