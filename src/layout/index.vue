8b71337f9f3687c1479a54fd9c35543e8c101058
lerzi <569259131@qq.com>
2023-06-11 20:23:32 +0800

    fix:全屏不生效

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,10 +3,11 @@ import Sider from './components/Sider/index.vue'
 import PageHeader from './components/PageHeader/index.vue'
 const appMain = ref(null)
 const { toggle } = useFullscreen(appMain)
+
 </script>
 
 <template>
-  <div>
+  <div ref="appMain">
     <n-layout has-sider position="absolute">
       <Sider></Sider>
       <n-layout>
