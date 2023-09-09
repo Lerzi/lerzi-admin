330c5cdb90bdbc950f10cc32f5f68070a1f5cf61
lerzi <569259131@qq.com>
2023-09-09 16:38:26 +0800

    feat:transition

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -14,11 +14,26 @@ const { toggle } = useFullscreen(appMain)
         <div h-full flex flex-col>
           <PageHeader @full="toggle"></PageHeader>
           <div w-full flex-1 p-6>
-            <router-view />
+            <router-view v-slot="{ Component, route }">
+              <transition name="fade" mode="out-in" :appear="true">
+                <component :is="Component" :key="route.fullPath" />
+              </transition>
+            </router-view>
           </div>
         </div>
-
       </n-layout>
     </n-layout>
   </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
